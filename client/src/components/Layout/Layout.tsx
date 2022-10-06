d3696a7021f84382f5c15dcffaf981f4a1548a7d
Derek Garnett <drkgrntt@gmail.com>
2022-10-05 23:03:49 -0500

    Build some page head and layout structure

---
+++ client/src/components/Layout/Layout.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react";
+import PageHead from "../PageHead";
+
+const Layout: FC = (props) => {
+  return (
+    <div>
+      <PageHead title="Store">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="language" content="en-us" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+      </PageHead>
+      <main>{props.children}</main>
+    </div>
+  );
+};
+
+export default Layout;
--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import { withApollo } from "../utils/withApollo";
+import Layout from "../components/Layout";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 };
 
 export default withApollo({ ssr: false })(App as any);
