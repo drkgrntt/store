d5c91710f9de456d3ceb770b4413ee180ff670e1
Derek Garnett <drkgrntt@gmail.com>
2022-10-05 21:41:50 -0500

    envs

--- server/src/env.d.ts
+++ server/src/env.d.ts
@@ -2,22 +2,16 @@ declare module "sanitize";
 
 declare namespace NodeJS {
   export interface ProcessEnv {
+    NODE_ENV: string;
     DATABASE_URL: string;
     PORT: string;
     JWT_SECRET: string;
     CLOUDINARY_CLOUD_NAME: string;
     CLOUDINARY_API_KEY: string;
     CLOUDINARY_API_SECRET: string;
-    // GMAIL: string;
-    // GMAIL_CLIENT_ID: string;
-    // GMAIL_CLIENT_SECRET: string;
-    // GMAIL_REFRESH_TOKEN: string;
     // APP_BASE_URL: string;
     // API_BASE_URL: string;
     // STRIPE_SECRET_KEY: string;
-    // STRIPE_MONTHLY_PRICE_ID: string;
-    // STRIPE_YEARLY_PRICE_ID: string;
-    // STRIPE_WEBHOOK_SECRET: string;
   }
 }
 
