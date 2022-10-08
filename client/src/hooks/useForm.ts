ed8fad18a0842a853c180373e2e18e9a921c918a
Derek Garnett <drkgrntt@gmail.com>
2022-10-08 13:55:04 -0500

    Improve validations

--- client/src/hooks/useForm.ts
+++ client/src/hooks/useForm.ts
@@ -6,7 +6,7 @@ export type Validation = {
   message: string;
   test: (value: InputValueType) => boolean;
 };
-export type Validations = Record<string, Validation>;
+export type Validations = Record<string, Validation | Validation[]>;
 
 export const getValue = (
   event: ChangeEvent<HTMLInputElement>
@@ -28,12 +28,27 @@ export const getValue = (
   }
 };
 
+const validate = (
+  value: InputValueType,
+  validations: Validation | Validation[]
+): string => {
+  if (!Array.isArray(validations)) {
+    validations = [validations];
+  }
+
+  for (const validation of validations) {
+    const isValid = validation.test(value);
+    if (!isValid) return validation.message;
+  }
+  return "";
+};
+
 export const emailRegex =
   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 export const useForm = <FormState extends Record<string, InputValueType>>(
   initialState: FormState,
-  validations?: Partial<Record<keyof FormState, Validation>>
+  validations?: Partial<Validations>
 ) => {
   const initialErrors: Partial<Record<keyof FormState, string>> = {};
 
@@ -61,8 +76,8 @@ export const useForm = <FormState extends Record<string, InputValueType>>(
         [name]: "Please use at least 6 characters in your password.",
       });
     } else if (validation) {
-      const isValid = validation.test(getValue(event));
-      setErrors({ ...errors, [name]: isValid ? "" : validation.message });
+      const message = validate(getValue(event), validation);
+      setErrors({ ...errors, [name]: message });
     } else if (errors[name]) {
       setErrors({ ...errors, [name]: "" });
     }
