368c526c5dcde083235f6a19fff186439fa377b6
Derek Garnett <drkgrntt@gmail.com>
2022-11-09 18:55:17 -0600

    attempt at cookie fix

--- server/src/middleware/handleTokens.ts
+++ server/src/middleware/handleTokens.ts
@@ -1,10 +1,11 @@
 import { NextFunction, Request, Response } from "express";
+import { __prod__ } from "../constants";
 
 export const handleTokens = (_: Request, res: Response, next: NextFunction) => {
   res.setToken = (value: string) => {
     res.cookie("token", value, {
-      sameSite: "none", //__prod__ ? "none" : "lax",
-      secure: true, //__prod__,
+      sameSite: __prod__ ? "none" : "lax",
+      secure: true,
       httpOnly: true,
       maxAge: 1000 * 60 * 60 * 24 * 30,
     });
@@ -12,8 +13,8 @@ export const handleTokens = (_: Request, res: Response, next: NextFunction) => {
 
   res.removeToken = () => {
     res.cookie("token", "a.b.c", {
-      sameSite: "none", //__prod__ ? "none" : "lax",
-      secure: true, //__prod__,
+      sameSite: __prod__ ? "none" : "lax",
+      secure: true,
       httpOnly: true,
       maxAge: 0,
     });
