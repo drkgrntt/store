5a4e2eea262d8da577106e7717b9306ca3256c10
Derek Garnett <drkgrntt@gmail.com>
2022-11-13 21:04:03 -0600

    remove unneeded

--- server/src/dataloaders/createImageIdsByProductLoader.ts
+++ server/src/dataloaders/createImageIdsByProductLoader.ts
@@ -2,28 +2,24 @@ import DataLoader from "dataloader";
 import { ProductImage } from "../models";
 
 export const createImageIdsByProductLoader = () => {
-  return new DataLoader<string, string[]>(
-    async (productIds: readonly string[]) => {
-      const images = await ProductImage.findAll({
-        attributes: ["id", "productId"],
-        where: {
-          productId: productIds,
-        },
-      });
+  return new DataLoader<string, string[]>(async (productIds) => {
+    const images = await ProductImage.findAll({
+      attributes: ["id", "productId"],
+      where: {
+        productId: productIds,
+      },
+    });
 
-      // Essentially is of type Record<productId: imageId[]>
-      const imageIdsMap = images.reduce((map, image) => {
-        map[image.productId] = images
-          .filter((ch) => image.productId === ch.productId)
-          .map((ch) => ch.id);
-        return map;
-      }, {} as Record<string, string[]>);
+    // Essentially is of type Record<productId: imageId[]>
+    const imageIdsMap = images.reduce((map, image) => {
+      map[image.productId] = images
+        .filter((ch) => image.productId === ch.productId)
+        .map((ch) => ch.id);
+      return map;
+    }, {} as Record<string, string[]>);
 
-      const sortedProductImageIds = productIds.map(
-        (id) => imageIdsMap[id] || []
-      );
+    const sortedProductImageIds = productIds.map((id) => imageIdsMap[id] || []);
 
-      return sortedProductImageIds;
-    }
-  );
+    return sortedProductImageIds;
+  });
 };
