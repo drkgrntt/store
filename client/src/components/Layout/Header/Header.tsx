fd50f93dcfd36542deb03c602c542224b2cfa3c3
Derek Garnett <drkgrntt@gmail.com>
2023-09-08 20:40:54 -0500

    new banner

--- client/src/components/Layout/Header/Header.tsx
+++ client/src/components/Layout/Header/Header.tsx
@@ -35,7 +35,7 @@ const Header: FC<Props> = () => {
       className={styles.header}
     >
       <Image
-        src="/images/banner.jpg"
+        src="/images/halloween-banner.jpg"
         width={2000}
         height={800}
         alt="Banner image"
